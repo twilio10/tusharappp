31676afdf094ae0fd5445b6fa1aa1d52a4457c5c
tush210896 <p.tusharrao420@gmail.com>
2024-02-18 03:37:17 -0800

    commit one

---
+++ src/main/java/Demo/Entity/Comment.java
@@ -0,0 +1,34 @@
+package Demo.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "comment")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "body")
+    private String body;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    // Getters and Setters
+    // ...
+}
---
+++ src/test/java/Demo/ManyToOneApplicationTests.java
@@ -0,0 +1,13 @@
+package Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManyToOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
